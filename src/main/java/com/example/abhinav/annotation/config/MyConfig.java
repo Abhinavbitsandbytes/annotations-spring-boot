c73b2b89dc8c092a4230d0fe710643343f96a1fe
Abhinav <abhinavkr197@gmail.com>
2025-12-31 14:26:53 +0530

    Configuration-2

--- src/main/java/com/example/abhinav/annotation/config/MyConfig.java
+++ src/main/java/com/example/abhinav/annotation/config/MyConfig.java
@@ -4,7 +4,7 @@ import com.example.abhinav.annotation.services.ProductService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class MyConfig {
 
     @Bean
@@ -12,3 +12,10 @@ public class MyConfig {
         return new ProductService();
     }
 }
+
+// if we comment out @Configuration annotation above, then Spring will not treat this class as a configuration class
+// and the bean defined inside it will not be created and managed by the Spring container.
+// Error - Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'com.example.abhinav.annotation.services.ProductService' available
+
+// Similarly, if we comment out @Bean annotation above the productService() method, then Spring will not create
+// and manage the ProductService bean.
--- src/main/java/com/example/abhinav/annotation/services/ProductService.java
+++ src/main/java/com/example/abhinav/annotation/services/ProductService.java
@@ -1,4 +1,8 @@
 package com.example.abhinav.annotation.services;
 
 public class ProductService {
+
+    public void createProduct(){
+        System.out.println("creating product");
+    }
 }
