9c1b007f70643b9018af73b58a56a4c800d2d78f
Abhinav <abhinavkr197@gmail.com>
2025-12-31 14:28:49 +0530

    xxxxcleanup

--- src/main/java/com/example/abhinav/annotation/SpringAnnotationApplication.java
+++ src/main/java/com/example/abhinav/annotation/SpringAnnotationApplication.java
@@ -14,25 +14,6 @@ public class SpringAnnotationApplication {
 	ProductService productService =	container.getBean(ProductService.class);
 	System.out.println(productService); // output : com.example.abhinav.annotation.services.ProductService@4784013e
 	productService.createProduct(); // output : creating product
-		// we have not created the object of ProductService using new keyword
-		// Spring container is managing the object creation and its lifecycle
-		// This is called Inversion of Control (IoC)
-		// Spring Framework is based on IoC principle
-		// Dependency Injection (DI) is a design pattern to implement IoC
-		// In DI, the objects are provided their dependencies at runtime by the container
-		// There are three types of DI : Constructor Injection, Setter Injection, Field Injection
-		// In this example, we are using Field Injection using @Autowired annotation
-		// We can also use @Component annotation to mark a class as a Spring Bean
-		// @Service, @Repository, @Controller are specializations of @Component
-		// They are used to mark a class as a Service, Repository, Controller respectively
-
-		// we are NOT using dependency injection here.
-		// We are manually getting the bean from the container
-		// In real world applications, we should use dependency injection
-
-//		What IS being used:
-//		Service Locator Pattern - The application is using the container to manually look up and retrieve beans
-//		Inversion of Control (IoC) - Yes, Spring is managing the object lifecycle, but that's different from Dependency Injection
 
 	}
 
--- src/main/java/com/example/abhinav/annotation/config/MyConfig.java
+++ src/main/java/com/example/abhinav/annotation/config/MyConfig.java
@@ -13,9 +13,3 @@ public class MyConfig {
     }
 }
 
-// if we comment out @Configuration annotation above, then Spring will not treat this class as a configuration class
-// and the bean defined inside it will not be created and managed by the Spring container.
-// Error - Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'com.example.abhinav.annotation.services.ProductService' available
-
-// Similarly, if we comment out @Bean annotation above the productService() method, then Spring will not create
-// and manage the ProductService bean.
