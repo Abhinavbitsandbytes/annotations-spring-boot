ae212873c8c43828f4cf8edc5736fbb0f379b519
Abhinav <abhinavkr197@gmail.com>
2025-12-31 15:38:37 +0530

    Component

--- src/main/java/com/example/abhinav/annotation/SpringAnnotationApplication.java
+++ src/main/java/com/example/abhinav/annotation/SpringAnnotationApplication.java
@@ -1,5 +1,6 @@
 package com.example.abhinav.annotation;
 
+import com.example.abhinav.annotation.services.CategoryService;
 import com.example.abhinav.annotation.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,14 +15,11 @@ public class SpringAnnotationApplication {
 	ProductService productService =	container.getBean(ProductService.class);
 	System.out.println(productService);
 	productService.createProduct();
+
+		CategoryService bean = container.getBean(CategoryService.class);
+		System.out.println(bean);
 	}
 
-	// we have created a sibling package named config where we have created a class MyConfig
-	// and annotated it with @Configuration
-	// when we run the app - Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'com.example.abhinav.annotation.services.ProductService' available
-	// this is because spring boot by default scans only the current package and its sub packages
-	// to make it scan the sibling package also we can use @ComponentScan annotation on the main application class
-	// @ComponentScan(basePackages = {"com.example.abhinav.annotation.config","com.example.abhinav.annotation.services"})
 
 
 }
